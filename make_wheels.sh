086e95acd6918729e272c891b4bcde295679c51a
Andrew Bogott <abogott@wikimedia.org>
2018-02-20 03:40:59 +0000

    updates for debian Stretch and labweb1001/1002
    - New, stretch-built wheels
    - Remove californium (Trusty) from deploy hosts
    - Work around a weird requirements.txt bug
    - Deploy on striker-uwsgi-stretch in WMCS
    
    Bug: T187743
    Change-Id: Idcad7069d3d5beadb2515c2ecaa2d6ac1eb63435

--- make_wheels.sh
+++ make_wheels.sh
@@ -17,6 +17,13 @@ mkdir -p $VENV
 virtualenv --python python3 $VENV || /bin/true
 $PIP install -r ${STRIKER}/requirements.txt
 $PIP freeze --local --requirement $REQUIREMENTS > $REQUIREMENTS
+
+# This is just a bug as far as I can tell. On Stretch
+#  this impossible requirement winds up in requirements.txt
+sed -i '/pkg-resources==0.0.0/d' ./requirements.txt
+
+
+
 $PIP install wheel
 $PIP wheel --find-links $WHEEL_DIR \
     --wheel-dir $WHEEL_DIR \
